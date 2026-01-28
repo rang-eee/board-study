5f04d06bd4cf419b7acacf66589903a439f5970c
pread <preadbear@gmail.com>
2026-01-28 16:13:25 +0900

    테스트 커밋 2

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,21 +1,22 @@
-//package com.example.demo.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//@Controller
-//public class IndexController {
-//
-//    // API ****
-//        //    @GetMapping("/")
-//        //    @ResponseBody
-////    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    @RequestMapping("/")
-//    public String index(){
-//        System.out.println("home indexController start1");
-//        return "/index";
-//    }
-//}
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    // API ****
+        //    @GetMapping("/")
+        //    @ResponseBody
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("title", "board test");
+        return "/index";
+    }
+}
